3b10f3cb17449df58d233e2877d955f4e5064c98
joshuayingwhat <joshuayingwhat@163.com>
2021-01-13 21:09:48 +0800

    添加全局文件

---
+++ globalFile.h
@@ -0,0 +1,23 @@
+//
+// Created by JoshuaYingWhat on 2021/1/13.
+//
+
+#ifndef ROOMRESERVATIONSYS_GLOBALFILE_H
+#define ROOMRESERVATIONSYS_GLOBALFILE_H
+
+//管理员文件
+#define ADMIN_FILE "admin.txt"
+
+//学生文件
+#define STUDENT_FILE "student.txt"
+
+//老师文件
+#define TEACHER_FILE "teacher.txt"
+
+//机房信息文件
+#define COMPUTER_ROOM_FILE "computerRoom.txt"
+
+//预约文件
+#define ORDER_FILE   "order.txt"
+
+#endif //ROOMRESERVATIONSYS_GLOBALFILE_H
