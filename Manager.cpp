04b5b0de8a6c551f983e1238922a764b93f21ab0
joshuayingwhat <joshuayingwhat@163.com>
2021-01-18 12:01:25 +0800

    管理员添加账号实现

--- Manager.cpp
+++ Manager.cpp
@@ -2,7 +2,10 @@
 // Created by JoshuaYingWhat on 2021/1/13.
 //
 
+#include <fstream>
 #include "Manager.h"
+
+#include "globalFile.h"
 
 Manager::Manager() {
 
@@ -40,6 +43,61 @@ void Manager::operMenu() {
 //添加账号
 void Manager::addAccount() {
 
+    cout << "请输入添加账号的类型" << endl;
+
+    cout << "1.学生账号" << endl;
+
+    cout << "2.老师账号" << endl;
+
+    string file_name;
+
+    string tips;
+
+    ofstream ofs;
+
+    int select = 0;
+
+    cin >> select;
+
+    if (select == 1) {
+
+        file_name = STUDENT_FILE;
+
+        tips = "请输入学号:  ";
+
+    } else if (select == 2) {
+
+        file_name = TEACHER_FILE;
+
+        tips = "请输入职工号：  ";
+    }
+
+    //打开文件
+    ofs.open(file_name, ios::out | ios::app);
+
+    int id;
+
+    string name;
+
+    string pwd;
+
+    cout << tips << endl;
+
+    cin >> id;
+
+    cout << "请输入姓名:  " << endl;
+
+    cin >> name;
+
+    cout << "请输入密码:  " << endl;
+
+    cin >> pwd;
+
+    ofs << id << " " << name << " " << pwd << " " << endl;
+
+    cout << "添加成功" << endl;
+
+    ofs.close();
 }
 
 //查看账号
